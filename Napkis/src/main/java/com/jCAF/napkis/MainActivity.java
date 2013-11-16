df9e3bb2050b33dab19d96d79155dd963d0a92b0
Alexander DeRieux <acd1797@vt.edu>
2013-11-16 18:23:30 -0500

    Update "MainActivity".

--- Napkis/src/main/java/com/jCAF/napkis/MainActivity.java
+++ Napkis/src/main/java/com/jCAF/napkis/MainActivity.java
@@ -18,11 +18,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -49,17 +44,5 @@ public class MainActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
